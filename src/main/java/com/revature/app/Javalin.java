0327bdb8ee365ad6f9ee23f232124687ae58b0bd
Henry Chen <69435913+Hyuria@users.noreply.github.com>
2020-12-17 11:11:01 -0500

    gitignore .idea files

--- src/main/java/com/revature/app/Javalin.java
+++ src/main/java/com/revature/app/Javalin.java
@@ -2,4 +2,14 @@ package com.revature.app;
 
 public class Javalin {
 
+    public static void main(String[] args) {
+        io.javalin.Javalin app = io.javalin.Javalin.create((config) -> {
+            config.addStaticFiles("/static");
+            config.enableCorsForAllOrigins();
+        });
+
+        app.start(8080);
+
+
+    }
 }
