93c942256cb6d0ef826d9109510437a95fc7b86b
Brodie Hufnagel <brodiehufnagel@gmail.com>
2020-12-18 17:17:27 -0500

    Added Status and Category Hibernate files

--- src/main/java/com/revature/data/CategoryHibernate.java
+++ src/main/java/com/revature/data/CategoryHibernate.java
@@ -1,33 +1,93 @@
 package com.revature.data;
 
 import com.revature.beans.Category;
+import com.revature.utils.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CategoryHibernate implements CategoryDAO{
+    private HibernateUtil hu = HibernateUtil.getHibernateUtil();
 
     @Override
     public Category add(Category c) {
-        return null;
+        Session s = hu.getSession();
+        Transaction tx = null;
+        try{
+            tx = s.beginTransaction();
+            s.save(c);
+            tx.commit();
+        }
+        catch (Exception e){
+            if(tx != null){
+                tx.rollback();
+            }
+        }
+        finally {
+            s.close();
+        }
+        return c;
     }
 
     @Override
     public Category getById(Integer id) {
-        return null;
+        Session s = hu.getSession();
+        Category c = s.get(Category.class, id);
+        s.close();
+        return c;
     }
 
     @Override
     public Set<Category> getAll() {
-        return null;
+        Session s = hu.getSession();
+        String query = "FROM Category";
+        Query<Category> q = s.createQuery(query, Category.class);
+        List<Category> categoryList = q.getResultList();
+        Set<Category> categorySet = new HashSet<>();
+        categorySet.addAll(categoryList);
+        s.close();
+        return categorySet;
     }
 
     @Override
     public void update(Category category) {
-
+        Session s = hu.getSession();
+        Transaction tx = null;
+        try{
+            tx = s.beginTransaction();
+            s.update(t);
+            tx.commit();
+        }
+        catch(Exception e){
+            if(tx != null){
+                tx.rollback();
+            }
+        }
+        finally{
+            s.close();
+        }
     }
 
     @Override
     public void delete(Category category) {
-
+        Session s = hu.getSession();
+        Transaction tx = null;
+        try{
+            tx = s.beginTransaction();
+            s.delete(t);
+            tx.commit();
+        }
+        catch(Exception e){
+            if(tx != null){
+                tx.rollback();
+            }
+        }
+        finally{
+            s.close();
+        }
     }
 }
--- src/main/java/com/revature/data/StatusHibernate.java
+++ src/main/java/com/revature/data/StatusHibernate.java
@@ -1,32 +1,87 @@
 package com.revature.data;
 
 import com.revature.beans.Status;
+import com.revature.utils.HibernateUtil;
 
 import java.util.Set;
 
 public class StatusHibernate implements StatusDAO{
+    private HibernateUtil hu = HibernateUtil.getHibernateUtil();
+
     @Override
     public Status getById(Integer id) {
-        return null;
+        Session s = hu.getSession();
+        Status status = s.get(Status.class, id);
+        s.close();
+        return status;
     }
 
     @Override
     public Set<Status> getAll() {
-        return null;
+        Session s = hu.getSession();
+        String query = "FROM Status";
+        Query<Status> q = s.createQuery(query, Status.class);
+        List<Status> statusList = q.getResultList();
+        Set<Status> statusSet = new HashSet<>();
+        statusSet.addAll(statusList);
+        s.close();
+        return statusSet;
     }
 
     @Override
     public void update(Status status) {
-
+        Session s = hu.getSession();
+        Transaction tx = null;
+        try{
+            tx = s.beginTransaction();
+            s.update(status);
+            tx.commit();
+        }
+        catch(Exception e){
+            if(tx != null){
+                tx.rollback();
+            }
+        }
+        finally{
+            s.close();
+        }
     }
 
     @Override
     public void delete(Status status) {
-
+        Session s = hu.getSession();
+        Transaction tx = null;
+        try{
+            tx = s.beginTransaction();
+            s.delete(status);
+            tx.commit;
+        }
+        catch(Exception e){
+            if(tx != null){
+                tx.rollback();
+            }
+        }
+        finally{
+            s.close();
+        }
     }
 
     @Override
-    public Status add(Status s) {
-        return null;
+    public Status add(Status status) {
+        Session s = hu.getSession();
+        Transaction tx = null;
+        try{
+            tx = s.beginTransaction();
+            s.save(status);
+            tx.commit();
+        }
+        catch(Exception e){
+            if(tx != null){
+                tx.rollback();
+            }
+        }
+        finally{
+            s.close();
+        }
     }
 }
