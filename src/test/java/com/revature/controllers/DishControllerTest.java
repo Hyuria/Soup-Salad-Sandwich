f1dfedb600146777226abf55a863fab66f7345ee
CaptKkrunk <49689117+CaptKkrunk@users.noreply.github.com>
2020-12-23 12:03:06 -0600

    controller test template

---
+++ src/test/java/com/revature/controllers/DishControllerTest.java
@@ -0,0 +1,75 @@
+package com.revature.controllers;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class DishControllerTest {
+	
+	@Test
+	void getAllDish() {
+		
+	}
+	
+	@Test
+	void addDish() {
+		
+	}
+	
+	@Test
+	void getDishById() {
+		
+	}
+	
+	@Test
+	void updateDish() {
+		
+	}
+	
+	@Test
+	void deleteDish(){
+		
+	}
+	
+	@Test
+	void getDishByCategory() {
+		
+	}
+	
+	@Test
+	void getAllComment() {
+		
+	}
+	
+	@Test
+	void getCommentByCommentId() {
+		
+	}
+	
+	@Test
+	void updateComment() {
+		
+	}
+	
+	@Test
+	void addComment() {
+		
+	}
+	
+	@Test
+	void deleteComment() {
+		
+	}
+	
+	@Test
+	void getRecentlyAddedDishes() {
+		
+	}
+	
+	@Test
+	void getHotDishes(){
+		
+	}
+
+}
---
+++ src/test/java/com/revature/controllers/UserControllerTest.java
@@ -0,0 +1,33 @@
+package com.revature.controllers;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserControllerTest {
+	
+	@Test
+	void checkLogin() {
+		
+	}
+	
+	@Test
+	void login() {
+		
+	}
+	
+	@Test
+	void register() {
+		
+	}
+	
+	@Test
+	void logout() {
+		
+	}
+	
+	void updateUser() {
+		
+	}
+
+}
