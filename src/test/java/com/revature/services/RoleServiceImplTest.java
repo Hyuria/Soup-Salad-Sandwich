d3cb6c722910f34e7aae0a4a0396c052ec432024
CaptKkrunk <49689117+CaptKkrunk@users.noreply.github.com>
2020-12-23 11:01:57 -0600

    Service test for role and status

--- src/test/java/com/revature/services/RoleServiceImplTest.java
+++ src/test/java/com/revature/services/RoleServiceImplTest.java
@@ -2,24 +2,41 @@ package com.revature.services;
 
 import org.junit.jupiter.api.Test;
 
+import com.revature.beans.Role;
+import com.revature.data.RoleDAO;
+import com.revature.data.RoleHibernate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class RoleServiceImplTest {
+	RoleDAO roleDAO = new RoleHibernate();
+	Role r = new Role();
+	Set<Role> roleSet = new HashSet<Role>();
+	
 
     @Test
     void addRole() {
+    	
     }
 
     @Test
     void getRoleById() {
+    	r = roleDAO.getById(1);
+    	assertEquals("user",r.getRole());
     }
 
     @Test
     void getAll() {
+    	roleSet = roleDAO.getAll();
+    	assertTrue(roleSet.size()>0);
     }
 
     @Test
     void updateRole() {
+    	
     }
 
     @Test
--- src/test/java/com/revature/services/StatusServiceImplTest.java
+++ src/test/java/com/revature/services/StatusServiceImplTest.java
@@ -2,9 +2,19 @@ package com.revature.services;
 
 import org.junit.jupiter.api.Test;
 
+import com.revature.beans.Status;
+import com.revature.data.StatusDAO;
+import com.revature.data.StatusHibernate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class StatusServiceImplTest {
+	Status s = new Status();
+	StatusDAO statusDAO = new StatusHibernate();
+	Set<Status> statusSet = new HashSet<>();
 
     @Test
     void addStatus() {
@@ -12,17 +22,29 @@ class StatusServiceImplTest {
 
     @Test
     void getStatusById() {
+    	s = statusDAO.getById(1);
+    	assertEquals("admin pending",s.getName());
     }
 
     @Test
     void getAll() {
+    	statusSet = statusDAO.getAll();
+    	assertTrue(statusSet.size()>0);
     }
 
     @Test
     void updateStatus() {
+    	s.setId(5);
+    	s.setName("temp2");
+    	statusDAO.update(s);
+    	assertEquals("temp2", statusDAO.getById(5).getName());
     }
 
     @Test
     void deleteStatus() {
+    	s.setId(5);
+    	s.setName("bob");
+    	statusDAO.delete(s);
+    	assertFalse(statusDAO.getById(5)!= null);
     }
 }
