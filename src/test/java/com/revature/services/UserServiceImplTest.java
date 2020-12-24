27a0cbead764d3a7d26063fa2d63594f711c88a0
benhsieh-dev <bqh5026@gmail.com>
2020-12-23 19:18:00 -0500

    Write JUnit tests for UserServiceImplTest.java and VoteService.java

--- src/test/java/com/revature/services/UserServiceImplTest.java
+++ src/test/java/com/revature/services/UserServiceImplTest.java
@@ -2,31 +2,70 @@ package com.revature.services;
 
 import org.junit.jupiter.api.Test;
 
+import com.revature.beans.User;
+import com.revature.data.RoleDAO;
+import com.revature.data.RoleHibernate;
+import com.revature.data.UserDAO;
+import com.revature.data.UserHibernate;
+import com.revature.exception.NonUniqueUsernameException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class UserServiceImplTest {
+    UserDAO userDAO = new UserHibernate();
+    RoleDAO roleDAO = new RoleHibernate();
+    User u = new User();
+    
+    Set<User> userSet = new HashSet<User>();
 
     @Test
-    void addUser() {
+    void addUser() throws NonUniqueUsernameException {
+    	u.setUsername("jessie");
+    	u.setPassword("jessie");	
+    	u.setRole(roleDAO.getById(1));
+    	u = userDAO.add(u);
+    	assertEquals("jessie", u.getUsername()); 
+        assertEquals("jessie", u.getPassword());
+        assertEquals(1, u.getRole());
     }
 
     @Test
     void getUserById() {
+        u = userDAO.getById(1);
+        assertEquals("jakeem", u.getUsername());
     }
 
     @Test
     void getAllUsers() {
+        userSet = userDAO.getAll();
+        assertTrue(userSet.size()>0);
     }
 
     @Test
     void getUserByUsername() {
+    	u = userDAO.getByUsername("caillou");
+    	assertEquals("caillou", u.getUsername());
     }
 
     @Test
     void updateUser() {
+        u.setId(22);
+        u.setUsername("tracey");
+        u.setPassword("tracey");
+        u.setRole(roleDAO.getById(1));
+        userDAO.update(u);
+        assertEquals("tracey", userDAO.getById(22).getUsername());
+        assertEquals("tracey", userDAO.getById(22).getPassword());
+        assertEquals(1, userDAO.getById(22).getRole());
     }
 
     @Test
     void deleteUser() {
+     	u.setId(22);
+    	userDAO.delete(u);
+    	assertFalse(userDAO.getById(22) !=null);  	
     }
 }
--- src/test/java/com/revature/services/VoteServiceImplTest.java
+++ src/test/java/com/revature/services/VoteServiceImplTest.java
@@ -2,31 +2,78 @@ package com.revature.services;
 
 import org.junit.jupiter.api.Test;
 
+import com.revature.beans.Vote;
+import com.revature.data.CategoryDAO;
+import com.revature.data.CategoryHibernate;
+import com.revature.data.DishDAO;
+import com.revature.data.DishHibernate;
+import com.revature.data.UserDAO;
+import com.revature.data.UserHibernate;
+import com.revature.data.VoteDAO;
+import com.revature.data.VoteHibernate;
+import com.revature.exception.AlreadyVotedException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class VoteServiceImplTest {
+	Vote v = new Vote();
+	VoteDAO voteDAO = new VoteHibernate();
+	DishDAO dishDAO = new DishHibernate();
+	UserDAO userDAO = new UserHibernate();
+	CategoryDAO categoryDAO = new CategoryHibernate();
+	
+	Set<Vote> voteSet = new HashSet<>();
 
     @Test
-    void addVote() {
+    void addVote() throws AlreadyVotedException {
+//    	v.setDish(dishDAO.getById(1));
+//    	v.setUser(userDAO.getById(2));
+//    	v.setCategory(categoryDAO.getById(1));
+//    	v = voteDAO.add(v);
+//    	assertEquals(1, v.getDish().getId()); 
+//        assertEquals(2, v.getUser().getId());
+//        assertEquals(1, v.getCategory().getId());  	
     }
 
     @Test
     void getVoteById() {
+        v = voteDAO.getById(1);
+        assertEquals(1, v.getId());
+        assertEquals(1, v.getDish().getId());
+        assertEquals(1, v.getUser().getId());
+        assertEquals(1, v.getCategory().getId());    
     }
 
     @Test
     void getAll() {
+    	voteSet = voteDAO.getAll();
+    	assertTrue(voteSet.size() > 0);
     }
 
     @Test
     void updateVote() {
+//    	v.setId(2);
+//      	v.setDish(dishDAO.getById(1));
+//    	v.setUser(userDAO.getById(3));
+//    	v.setCategory(categoryDAO.getById(4));
+//    	voteDAO.update(v);
+//    	assertEquals(1, v.getDish().getId()); 
+//    	assertEquals(3, v.getUser().getId());
+//    	assertEquals(4, v.getCategory().getId());  
     }
 
     @Test
     void categoryVote() {
+    	// what's this?
     }
 
     @Test
     void deleteVote() {
+    	v.setId(2);
+    	voteDAO.delete(v);
+    	assertFalse(voteDAO.getById(2) !=null);  	
     }
 }
